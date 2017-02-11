f430c11325a0c12041aa1088e40e67061ba4a8e1
Imanou Petit <imanoupetit@yahoo.fr>
2017-02-11 18:02:50 +0100

    Commit again

--- AudioEngineSimpleApp/PlayController.swift
+++ AudioEngineSimpleApp/PlayController.swift
@@ -4,22 +4,13 @@ import AVKit
 
 class PlayController: UIViewController {
     
-    let url = URL(fileURLWithPath: NSTemporaryDirectory().appending("test.caf"))
     let player = AVPlayer()
     var sourceUrl: URL? {
         didSet {
-            // Update player with new source audio file or nothing if sourceUrl is nil
+            // Update player with new source audio file or nothing (if sourceUrl is nil)
             let playerItem: AVPlayerItem? = sourceUrl.map { AVPlayerItem(url: $0) }
             player.replaceCurrentItem(with: playerItem)
         }
-    }
-    
-    // MARK: View Controller
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        sourceUrl = url
     }
     
     // MARK: - Navigation
--- AudioEngineSimpleApp/RecordController.swift
+++ AudioEngineSimpleApp/RecordController.swift
@@ -5,50 +5,60 @@ import AVFoundation
 class RecordController: UIViewController {
     
     @IBOutlet weak var recordButton: UIButton!
-    let url = URL(fileURLWithPath: NSTemporaryDirectory().appending("test.caf"))
+    let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("test.caf")
     var engine = AVAudioEngine()
-    var file: AVAudioFile?
     
+    // MARK: - View lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         recordButton.setTitle("Record", for: .normal)
         
+        let file: AVAudioFile
         do {
             file = try AVAudioFile(forWriting: url, settings: engine.inputNode!.outputFormat(forBus: 0).settings)
         } catch {
             print("Error: \(error)")
+            return
         }
         
         engine.inputNode?.installTap(onBus: 0, bufferSize: 1024, format: engine.inputNode!.outputFormat(forBus: 0)) { (buffer, time) -> Void in
             do {
-                try self.file?.write(from: buffer)
+                try file.write(from: buffer)
             } catch {
                 print("Error: \(error)")
             }
         }
     }
     
-    @IBAction func record(_ sender: AnyObject) {
-        if engine.isRunning {
-            // Toggle button title
-            recordButton.setTitle("Record", for: .normal)
+    @IBAction func toggleAction(_ sender: AnyObject) {
+        engine.isRunning ? stopRecording() : startRecording()
+    }
+    
+    func startRecording() {
+        do {
+            // Start engine
+            try engine.start()
             
-            // Stop engine and push PlayController
-            engine.inputNode?.removeTap(onBus: 0)
-            guard let playerController = storyboard?.instantiateViewController(withIdentifier: "PlayerController") as? PlayController else { return }
-            navigationController?.pushViewController(playerController, animated: true)
-        } else {
             // Toggle button title
             recordButton.setTitle("Stop", for: .normal)
-            
-            // Start engine
-            do {
-                try engine.start()
-            } catch {
-                print("Error: \(error)")
-            }
+        } catch {
+            print("Error: \(error)")
         }
+    }
+    
+    func stopRecording() {
+        // Remove tap and stop engine
+        engine.stop()
+        
+        // Toggle button title
+        recordButton.setTitle("Record", for: .normal)
+        
+        // Present PlayController
+        guard let playerController = storyboard?.instantiateViewController(withIdentifier: "PlayerController") as? PlayController else { return }
+        playerController.sourceUrl = url
+        navigationController?.pushViewController(playerController, animated: true)
     }
     
 }
